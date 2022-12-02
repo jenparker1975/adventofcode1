8a3ffd68004e38423d0f77e07bacf1e14a32a937
Jen Parker <jenparker1975@gmail.com>
2022-12-02 16:55:27 -0500

    day 2

---
+++ DayOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode1
+{
+    public class DayOne
+    {
+        public static void CalculateCalories()
+        {
+            var elvesAndCalories = new Dictionary<int, int>();
+            var elf = 1;
+
+            var lines = File.ReadAllLines(@"D:\source\adventofcode1\elves.txt");
+            elvesAndCalories.Add(elf, 0);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elf++;
+                    elvesAndCalories.Add(elf, 0);
+                    continue;
+                }
+
+                if (!int.TryParse(line, out int calories))
+                    continue;
+
+                elvesAndCalories[elf] += calories;
+            }
+
+            var elfWithMost = elvesAndCalories.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+            Console.WriteLine("{0} elf had {1} calories", elfWithMost, elvesAndCalories[elfWithMost]);
+
+
+            Console.WriteLine(elvesAndCalories.OrderByDescending(x => x.Value).Take(3).Sum(s => s.Value));
+        }
+    }
+}
---
+++ DayTwo.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode1
+{
+    public class DayTwo
+    {
+        public static void CalculateScore1()
+        {
+            var lines = File.ReadAllLines(@"D:\source\adventofcode1\rockpaperscissors.txt");
+            int totalScore = 0;
+            foreach (var line in lines)
+            {
+                var chars = line.ToCharArray();
+
+                switch (chars[0])
+                {
+                    //opponent picks Rock
+                    case 'A':
+                        if (chars[2] == 'X') //rock
+                        {
+                            totalScore += 1 + 3;
+                        }
+                        else if (chars[2] == 'Y') //paper
+                        {
+                            totalScore += 2 + 6;
+                        }
+                        else //scissors
+                        {
+                            totalScore += 3 + 0;
+                        }
+
+                        break;
+
+                    //opponent picks Paper
+                    case 'B':
+                        if (chars[2] == 'X') //rock
+                        {
+                            totalScore += 1 + 0;
+                        }
+                        else if (chars[2] == 'Y') //paper
+                        {
+                            totalScore += 2 + 3;
+                        }
+                        else //scissors
+                        {
+                            totalScore += 3 + 6;
+                        }
+                        break;
+                        ;
+
+                    //opponent picks scissors
+                    case 'C':
+                        if (chars[2] == 'X') //rock
+                        {
+                            totalScore += 1 + 6;
+                        }
+                        else if (chars[2] == 'Y') //paper
+                        {
+                            totalScore += 2 + 0;
+                        }
+                        else //scissors
+                        {
+                            totalScore += 3 + 3;
+                        }
+
+                        break;
+                }
+            }
+            Console.WriteLine(totalScore);
+        }
+
+        public static void CalculateScore2()
+        {
+            var lines = File.ReadAllLines(@"D:\source\adventofcode1\rockpaperscissors.txt");
+            int totalScore = 0;
+            foreach (var line in lines)
+            {
+                var chars = line.ToCharArray();
+
+                switch (chars[0])
+                {
+                    //opponent picks Rock
+                    case 'A':
+                        if (chars[2] == 'X') //I need to lose, so I pick scissors
+                        {
+                            totalScore += 3 + 0;
+                        }
+                        else if (chars[2] == 'Y') //I need a drawer, so I pick rock
+                        {
+                            totalScore += 1 + 3;
+                        }
+                        else //I need to win so I pick paper
+                        {
+                            totalScore += 2 + 6;
+                        }
+
+                        break;
+
+                    //opponent picks Paper
+                    case 'B':
+                        if (chars[2] == 'X') //I need to lose, so I pick rock
+                        {
+                            totalScore += 1 + 0;
+                        }
+                        else if (chars[2] == 'Y') //I need a drawer so I pick paper
+                        {
+                            totalScore += 2 + 3;
+                        }
+                        else //I need to win so I pick scissors
+                        {
+                            totalScore += 3 + 6;
+                        }
+                        break;
+                        ;
+
+                    //opponent picks scissors
+                    case 'C':
+                        if (chars[2] == 'X') //I need to lose, so I pick paper
+                        {
+                            totalScore += 2 + 0;
+                        }
+                        else if (chars[2] == 'Y') //I need a drawer so I pick scissors
+                        {
+                            totalScore += 3 + 3;
+                        }
+                        else //I need to win so I pick rock
+                        {
+                            totalScore += 1 + 6;
+                        }
+
+                        break;
+                }
+            }
+            Console.WriteLine(totalScore);
+        }
+    }
+}
